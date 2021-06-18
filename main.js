37d212eb0e91cc573079549b1dd02540d4699a6b
ksy70029 <80887385+ksy70029@users.noreply.github.com>
2021-06-18 15:30:16 +0900

    JS : Transparent home

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Make navbar transparent when it is on the top
-const navbar = document.querySelector('#navbar')
+const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height
 document.addEventListener('scroll', () => {
     if (window.scrollY > navbarHeight) {
@@ -28,7 +28,27 @@ homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
 
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight
+})
+
+
+
+
+
+
+
+
+
+
+
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
 }
+
+
