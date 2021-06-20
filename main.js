48732e552af2e57622d58122dba1213228c350ad
ksy70029 <80887385+ksy70029@users.noreply.github.com>
2021-06-20 09:52:19 +0900

    JS : Project filtering

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight
-})
+});
 
 //Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector(".arrow-up")
@@ -50,7 +50,28 @@ arrowUp.addEventListener('click', () => {
     scrollIntoView('#home');
 });
 
-
+//Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if (filter === '*' || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    }, 300);
+});
 
 
 
